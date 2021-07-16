e3e061086d38b71fb6d68bd020bdfeb9eaef4d1d
DavidCavallaro <68825684+DavidCavallaro@users.noreply.github.com>
2021-07-16 22:49:43 +0200

    Update

--- commands/kick.js
+++ commands/kick.js
@@ -3,21 +3,21 @@ const Discord = require('discord.js');
 module.exports = {
   name: `kick`,
   async run (client, message, args) {
-		if(!message.member.hasPermission("KICK_MEMBERS")) return message.channel.send(`You cannot execute this command! You don\'t have the necessary permissions!`)
-    if(!message.guild.me.hasPermission("KICK_MEMBERS")) return message.channel.send(`You cannot execute this command! You don\'t have the necessary permissions!`)
+		if(!message.member.hasPermission("KICK_MEMBERS")) return message.channel.send(`Non puoi fare questo comando! Non hai il permesso di "Espellere membri".`)
+    if(!message.guild.me.hasPermission("KICK_MEMBERS")) return message.channel.send(`Non puoi fare questo comando! Non hai il permesso di "Espellere membri".`)
 
     const member = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
 
-    if(!args[0]) return message.channel.send(`${message.author}, please specify a user to perform the action on.`);
+    if(!args[0]) return message.channel.send(`${message.author}, inserisci un utente.`);
 
-    if(!member) return message.channel.send(`I can\'t find this user. I\'m sorry.`);
-    if(!member.kickable) return message.channel.send(`I cannot ban this user. He may have a higher role than mine or I may not have the necessary permissions.`);
+    if(!member) return message.channel.send(`Non ho trovato nessun utente con questo nome.`);
+    if(!member.kickable) return message.channel.send(`Non posso espellere questo utente. Potrebbe avere un ruolo più alto del mio o potrei non avere i permessi necessari.`);
 
-    if(member.id === message.author.id) return message.channel.send(`You can\'t kick yourself!`);
+    if(member.id === message.author.id) return message.channel.send(`Non puoi kickarti da solo!`);
 
     let reason = args.slice(1).join(" ");
 
-    if(reason === undefined) reason = `Not specified`;
+    if(reason === undefined) reason = `Non specificato`;
 
     member.kick(reason)
     .catch(err => {
