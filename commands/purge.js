c33e9b015449c841caeb5b0e122b789f8dfaa1e6
DavidCavallaro <68825684+DavidCavallaro@users.noreply.github.com>
2021-07-16 22:43:44 +0200

    Update

---
+++ commands/purge.js
@@ -0,0 +1,16 @@
+module.exports = {
+    name: `purge`,
+    async run (client, message, args) {
+          const clearargs = args[0]
+        const amount = args.join(" ");
+  
+        if(!amount) return message.reply('inserisci un numero di messaggi da eliminare!')
+  
+        if(amount > 100) return message.reply(`I can delete a maximum of 100 messages!`)
+  
+        if(amount < 1) return message.reply(`you must specify a sum of at least one message!`)
+  
+        await message.channel.messages.fetch({limit: amount}).then(messages => {message.channel.bulkDelete(messages)});
+        message.channel.send(`Ho eliminato ${clearargs} messaggi.`).then(msg => msg.delete({timeout:5000}))
+    }
+  }
