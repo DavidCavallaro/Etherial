afa9bbcb88681a8f656ff0cffaea3bebe9403e25
DavidCavallaro <68825684+DavidCavallaro@users.noreply.github.com>
2021-07-16 22:45:38 +0200

    Update

---
+++ commands/clear.js
@@ -0,0 +1,16 @@
+module.exports = {
+    name: `clear`,
+    async run (client, message, args) {
+          const clearargs = args[0]
+        const amount = args.join(" ");
+  
+        if(!amount) return message.reply('inserisci un numero di messaggi da eliminare!')
+  
+        if(amount > 100) return message.reply(`Posso eliminare un massimo di 100 messaggi!`)
+  
+        if(amount < 1) return message.reply(`devi specificare un numero valido!`)
+  
+        await message.channel.messages.fetch({limit: amount}).then(messages => {message.channel.bulkDelete(messages)});
+        message.channel.send(`Ho eliminato ${clearargs} messaggi.`).then(msg => msg.delete({timeout:5000}))
+    }
+  }
